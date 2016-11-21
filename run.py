42de93d43d422b8bab8c5d17dd448fc8c85ccd86
Miklos Homolya <m.homolya14@imperial.ac.uk>
2016-11-21 10:53:42 +0000

    save work so it will not get lost

---
+++ run.py
@@ -0,0 +1,229 @@
+from __future__ import absolute_import, print_function, division
+from six import iteritems
+from six.moves import map, range, zip
+
+from functools import reduce
+import itertools
+import logging
+from time import time
+
+import numpy
+
+import ufl
+import ffc
+
+
+ffc_logger = logging.getLogger("FFC")
+ffc_logger.setLevel(logging.WARNING)
+
+
+try:
+    ufl.Mesh
+    ufl.FunctionSpace
+
+    def ufl_cell(domain):
+        return domain.ufl_cell()
+
+except AttributeError:
+    def mesh(element):
+        return ufl.Domain(ufl.Coefficient(element))
+
+    def function_space(domain, element):
+        return element.reconstruct(domain=domain)
+
+    ufl.Mesh = mesh
+    ufl.FunctionSpace = function_space
+    ufl.Domain.ufl_cell = ufl.Domain.cell
+
+
+domains = [ufl.Mesh(ufl.VectorElement('P', ufl.triangle, 1)),
+           ufl.Mesh(ufl.VectorElement('P', ufl.tetrahedron, 1))]
+
+
+def helmholtz(domain, q, p, nf=0):
+    V = ufl.FunctionSpace(domain, ufl.FiniteElement('P', domain.ufl_cell(), q))
+    P = ufl.FunctionSpace(domain, ufl.FiniteElement('P', domain.ufl_cell(), p))
+    u = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+    f = [ufl.Coefficient(P) for _ in range(nf)]
+    it = ufl.dot(ufl.grad(v), ufl.grad(u)) + 1.0*v*u
+    return reduce(ufl.inner, f + [it])*ufl.dx
+
+
+def elasticity(domain, q, p, nf=0):
+    V = ufl.FunctionSpace(domain, ufl.VectorElement('P', domain.ufl_cell(), q))
+    P = ufl.FunctionSpace(domain, ufl.VectorElement('P', domain.ufl_cell(), p))
+    u = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+
+    def eps(v):
+        return ufl.grad(v) + ufl.transpose(ufl.grad(v))
+    it = 0.25*ufl.inner(eps(v), eps(u))
+    f = [ufl.Coefficient(P) for _ in range(nf)]
+    return reduce(ufl.inner, f + [it])*ufl.dx
+
+
+def hyperelasticity(domain, q, p, nf=0):
+    V = ufl.FunctionSpace(domain, ufl.VectorElement('P', domain.ufl_cell(), q))
+    P = ufl.FunctionSpace(domain, ufl.VectorElement('P', domain.ufl_cell(), p))
+    v = ufl.TestFunction(V)
+    du = ufl.TrialFunction(V)  # Incremental displacement
+    u = ufl.Coefficient(V)     # Displacement from previous iteration
+    B = ufl.Coefficient(V)     # Body force per unit mass
+    # Kinematics
+    I = ufl.Identity(domain.ufl_cell().topological_dimension())
+    F = I + ufl.grad(u)        # Deformation gradient
+    C = F.T*F                  # Right Cauchy-Green tensor
+    E = (C - I)/2              # Euler-Lagrange strain tensor
+    E = ufl.variable(E)
+    # Material constants
+    mu = ufl.Constant(domain)  # Lame's constants
+    lmbda = ufl.Constant(domain)
+    # Strain energy function (material model)
+    psi = lmbda/2*(ufl.tr(E)**2) + mu*ufl.tr(E*E)
+    S = ufl.diff(psi, E)       # Second Piola-Kirchhoff stress tensor
+    PK = F*S                   # First Piola-Kirchoff stress tensor
+    # Variational problem
+    it = ufl.inner(PK, ufl.grad(v)) - ufl.inner(B, v)
+    f = [ufl.Coefficient(P) for _ in range(nf)]
+    return ufl.derivative(reduce(ufl.inner,
+                                 list(map(ufl.div, f)) + [it])*ufl.dx, u, du)
+
+
+def holzapfel_ogden(mesh, q, p, nf=0):
+    from ufl import (Constant, VectorConstant, Identity, Coefficient,
+                     TestFunction, det, diff, dot, exp, grad, inner,
+                     ln, tr, variable)
+
+    # Define some random parameters
+    lamda = Constant(mesh)
+    a = Constant(mesh)
+    b = Constant(mesh)
+    a_s = Constant(mesh)
+    b_s = Constant(mesh)
+    a_f = Constant(mesh)
+    b_f = Constant(mesh)
+    a_fs = Constant(mesh)
+    b_fs = Constant(mesh)
+
+    # For more fun, make these general vector fields rather than
+    # constants:
+    e_s = VectorConstant(mesh)
+    e_f = VectorConstant(mesh)
+
+    # Define the isochoric energy contribution
+    def isochoric(F):
+        C = F.T*F
+
+        I_1 = tr(C)
+        I4_f = dot(e_f, C*e_f)
+        I4_s = dot(e_s, C*e_s)
+        I8_fs = dot(e_f, C*e_s)
+
+        def cutoff(x):
+            return 1.0/(1.0 + exp(-(x - 1.0)*30.0))
+
+        def scaled_exp(a0, a1, argument):
+            return a0/(2.0*a1)*(exp(b*argument) - 1)
+
+        E_1 = scaled_exp(a, b, I_1 - 3.)
+
+        E_f = cutoff(I4_f)*scaled_exp(a_f, b_f, (I4_f - 1.)**2)
+        E_s = cutoff(I4_s)*scaled_exp(a_s, b_s, (I4_s - 1.)**2)
+        E_3 = scaled_exp(a_fs, b_fs, I8_fs**2)
+
+        E = E_1 + E_f + E_s + E_3
+        return E
+
+    # Define mesh and function space
+    V = ufl.FunctionSpace(mesh, ufl.VectorElement("CG", mesh.ufl_cell(), q))
+    u = Coefficient(V)
+    v = TestFunction(V)
+
+    # Misc elasticity related tensors and other quantities
+    I = Identity(mesh.ufl_cell().topological_dimension())
+    F = grad(u) + I
+    F = variable(F)
+    J = det(F)
+    Fbar = J**(-1.0/3.0)*F
+
+    # Define energy
+    E_volumetric = lamda*0.5*ln(J)**2
+    psi = isochoric(Fbar) + E_volumetric
+
+    # Find first Piola-Kircchoff tensor
+    P = diff(psi, F)
+
+    # Define the variational formulation
+    it = inner(P, grad(v))
+
+    P = ufl.FunctionSpace(mesh, ufl.FiniteElement('P', mesh.ufl_cell(), p))
+    f = [ufl.Coefficient(P) for _ in range(nf)]
+    return ufl.derivative(reduce(ufl.inner,
+                                 list(map(ufl.div, f)) + [it])*ufl.dx, u)
+
+
+# forms = [helmholtz, elasticity, hyperelasticity, holzapfel_ogden]
+forms = [holzapfel_ogden]
+
+
+def tsfc_compile_form(form, parameters=None):
+    import tsfc
+    kernel, = tsfc.compile_form(form, parameters=parameters)
+    return kernel.ast
+
+
+def ffc_compile_form(form, parameters=None):
+    # Use jit=True to disable element compilation.
+    # Makes time comparison fairer.
+    if parameters is None:
+        parameters = ffc.default_jit_parameters()
+    else:
+        _ = ffc.default_jit_parameters()
+        _.update(parameters)
+        parameters = _
+
+    code_h, code_c = \
+        ffc.compile_form([form], parameters=parameters, jit=True)
+    return code_c
+
+
+compilers = {
+    'tensor': lambda form: ffc_compile_form(
+        form,
+        parameters={'representation': 'tensor', 'quadrature_degree': 6}
+    ),
+    # 'quadrature': lambda form: ffc_compile_form(
+    #     form,
+    #     parameters={'representation': 'quadrature', 'quadrature_degree': 6}
+    # ),
+    'uflacs': lambda form: ffc_compile_form(
+        form,
+        parameters={'representation': 'uflacs', 'quadrature_degree': 6}
+    ),
+    'tsfc-quick': lambda form: tsfc_compile_form(
+        form,
+        parameters={'unroll_indexsum': False, 'quadrature_degree': 6}
+    ),
+    'tsfc-default': lambda form: tsfc_compile_form(
+        form,
+        parameters={'quadrature_degree': 6}
+    ),
+}
+
+
+def run():
+    for domain, form, (name, compiler) in itertools.product(domains, forms, iteritems(compilers)):
+        if form in [hyperelasticity, holzapfel_ogden] and name == 'tensor':
+            continue
+        f = form(domain, 2, 2)
+        number = 1
+        start = time()
+        ends = [None] * number
+        for i in range(number):
+            compiler(f)
+            ends[i] = time()
+        times = numpy.diff([start] + ends)
+        times.sort()
+        t = times[:3].mean()
+        print(form.__name__, domain.ufl_cell().topological_dimension(), name, t)
